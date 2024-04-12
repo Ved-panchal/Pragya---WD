20cb2cf8c510b40ad3ca9395ba7763c1d1922d13
Praagya288 <praagyajain2828@gmail.com>
2024-04-12 11:28:20 +0530

    made a project

--- react js/7-currencyConverter/src/App.jsx
+++ react js/7-currencyConverter/src/App.jsx
@@ -3,11 +3,11 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
- 
+  
 
   return (
     <>
-     <h1 className='text-3xl bg-orange-500'>currency app with chai</h1>
+    <h1 className='text-3xl bg-orange-500 '>learning rezct from chai aur code</h1>
     </>
   )
 }
